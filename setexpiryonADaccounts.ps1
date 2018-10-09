b5ac91c39f19c4ed6794d9fd4dfb9b7e29cb1e16
delroid <delroy.mitchell@gmail.com>
2018-10-09 23:26:20 +0100

    first commit

---
+++ setexpiryonADaccounts.ps1
@@ -0,0 +1,17 @@
+﻿
+#create array of users
+
+#domain admins array
+$users = Get-ADGroupMember -Identity "domain admins"  |select samaccountname
+
+#all users in array
+#$users = Get-ADGroupMember -Identity "domain admins"  |select samaccountname
+
+
+foreach ($user in $users) {            
+#Search in specified OU and Update existing attributes            
+ Get-ADUser -Filter "SamAccountName -eq '$($user.samaccountname)'" -Properties * | Set-ADUser -PasswordNeverExpires $true
+ #|            
+
+           
+}
