ed54807f80ccfe4ef024f5498c912435fc8ad439
delroid <delroy.mitchell@gmail.com>
2018-10-09 23:31:38 +0100

    test

--- setexpiryonADaccounts.ps1
+++ setexpiryonADaccounts.ps1
@@ -9,7 +9,7 @@ $users = Get-ADGroupMember -Identity "domain admins"  |select samaccountname
 
 
 foreach ($user in $users) {            
-#Search in specified OU and Update existing attributes            
+#For each user set password not to expire       
  Get-ADUser -Filter "SamAccountName -eq '$($user.samaccountname)'" -Properties * | Set-ADUser -PasswordNeverExpires $true
  #|            
 
