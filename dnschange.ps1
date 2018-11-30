189f928d0d672e2333b47354c699114eceb4a466
delroid <delroy.mitchell@gmail.com>
2018-11-30 16:45:18 +0000

    Add files via upload

---
+++ dnschange.ps1
@@ -0,0 +1,26 @@
+﻿ # $dnsservers = "8.8.8.8" googletester
+#clear dns variable
+$dnsservers = ""
+$computers = Get-Content complist.txt
+foreach ($comp in $computers)
+{
+
+	$adapters = gwmi -q "select * from win32_networkadapterconfiguration where ipenabled='true'" -ComputerName $comp
+	foreach ($adapter in $adapters)
+	{
+		$adapter.setDNSServerSearchOrder($dnsservers)
+	}
+	
+
+            $dnsservers = "8.8.8.8"
+$computers = Get-Content complist.txt
+foreach ($comp in $computers)
+{
+
+	$adapters = gwmi -q "select * from win32_networkadapterconfiguration where ipenabled='true'" -ComputerName $comp
+	foreach ($adapter in $adapters)
+	{
+		$adapter.setDNSServerSearchOrder($dnsservers)
+	}
+}
+}
